7c294773ce51235a93c7dd305adea3281a769ee8
Robin de Rooij <rderooij685@gmail.com>
2022-04-04 18:15:43 +0200

    Use correct package-lock instead of github

--- webpack.config.js
+++ webpack.config.js
@@ -47,11 +47,10 @@ module.exports = (env, argv) => ({
           options:{
             presets: [
               ["@babel/preset-env"],
-              "@babel/preset-react"
+              ["@babel/preset-react", {
+                "runtime": "automatic",
+              }]
             ],
-            plugins: [
-              "@babel/plugin-transform-react-jsx"
-            ]
           }
         },
       },
