fd5e9135ed5dd48bdf61e64787ea0d79654da51d
Kitty Jose <kitty@terminusdb.com>
2023-02-28 16:44:02 +0000

    adding CDRI_INDIA (#39)

--- src/constants.js
+++ src/constants.js
@@ -4,5 +4,6 @@ export const DOMINICA_TEAM="Dominica"
 export const SANTA_ANA_TEAM="SantaAna"
 export const MIAMI_TEAM="Miami"
 export const MEXICO_TEAM="MexicoCity"
+export const CDRI_INDIA_TEAM="CDRI_INDIA"
 
-export const teamIdList = [DOMINICA_TEAM, MIAMI_TEAM, SANTA_ANA_TEAM, MEXICO_TEAM]
+export const teamIdList = [DOMINICA_TEAM, MIAMI_TEAM, SANTA_ANA_TEAM, MEXICO_TEAM, CDRI_INDIA_TEAM]
